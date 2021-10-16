ca496dd4d2cf4c9b60c464e12407e977562a94e9
kelvys B. Pantaleão <contato@kelvysb.com>
2021-10-15 21:20:26 -0300

    Update to .net 6 Added script project

---
+++ HARIA/HARIA.Domain/Entities/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HARIA.Domain.Entities
+{
+    public class Schedule: EntityBase    
+    {
+        public DateTime CreationTime { get; set; }
+        public DateTime ExecTime { get; set; }
+        public string Command { get; set; }
+    }
+}
